515f89f4d91a962456ca8f43b2f5331138dc1204
AlexMuia31 <alexmuia31@gmail.com>
2023-12-16 14:10:00 +0300

    get crypto

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,32 @@
 import { AppBar, Box, Container, Toolbar } from "@mui/material";
 import React from "react";
 import { ConnectWallet } from "@thirdweb-dev/react";
+import { GeneralTypo } from "./Typographies";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
     <Box>
       <AppBar sx={{ background: "#040C18" }}>
         <Toolbar>
-          <Container maxWidth="xl">
-            {" "}
-            <ConnectWallet />
+          <Container
+            maxWidth="xl"
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Link href="/" style={{ textDecoration: "none" }}>
+              <GeneralTypo>Home</GeneralTypo>
+            </Link>
+            <Link href="/transactions" style={{ textDecoration: "none" }}>
+              <GeneralTypo>Transact</GeneralTypo>
+            </Link>
+            <GeneralTypo sx={{ display: { xs: "none", sm: "flex" } }}>
+              About
+            </GeneralTypo>
+            <ConnectWallet className="buttonColor" />
           </Container>
         </Toolbar>
       </AppBar>
